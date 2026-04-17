37a567d0aef41fd6252980646842df483ec2534a
Ulf Dellbrügge <xintamosaik@gmail.com>
2026-04-17 17:49:18 +0200

    better build script

--- build.sh
+++ build.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 set -eu
+
+mkdir -p dist
+
 zig build-exe src/wasm.zig \
   -target wasm32-freestanding \
   -fno-entry \
   --export=add \
-  -femit-bin=wasm.wasm
+  -femit-bin=dist/wasm.wasm
 
-mv wasm.wasm dist/wasm.wasm
+ 
