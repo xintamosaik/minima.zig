99900ac14e9c1a4c79efb8e58dc5c153b238a35a
Ulf Dellbrügge <xintamosaik@gmail.com>
2026-04-17 23:43:08 +0200

    add button to trigger tick function and update counter display

--- dist/index.mjs
+++ dist/index.mjs
@@ -2,11 +2,21 @@
 const out = document.getElementById("out");
 
 try {
-    const response = await fetch("./wasm.wasm");
+    const response = await fetch("./index.wasm");
     const { instance } = await WebAssembly.instantiateStreaming(response);
     console.log(instance);
+    const add = instance.exports.add;
+    const tick = instance.exports.tick;
+    const getCounter = instance.exports.getCounter;
+    const button = document.getElementById("tick");
+    button.addEventListener("click", () => {
+        tick();
+        const counter = getCounter();
+        out.textContent = `Counter: ${counter}`;
+    });
     const result = instance.exports.add(3, 7);
     out.textContent = `3 + 7 = ${result}`;
 } catch (err) {
     out.textContent = `Failed to load wasm: ${err}`;
 }
+
--- src/index.zig
+++ src/index.zig
@@ -1,8 +1,17 @@
+var counter: i32 = 0;
+
 export fn add(a: i32, b: i32) i32 { 
     return a + b; 
 }
 
-export fn tick() void {}
+export fn tick() void {
+    counter += 1;
+}
+
+
+export fn getCounter() i32 {
+    return counter;
+}
 
 export fn width() i32 {
     return 128;
