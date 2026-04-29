93cacf6da5f0525e9eaa9415d05f919288acb5a4
Ulf Dellbrügge <xintamosaik@gmail.com>
2026-04-29 10:55:23 +0200

    have another scnene in a separate file

--- src/index.zig
+++ src/index.zig
@@ -11,6 +11,7 @@ const patterns_world = @import("patterns_world.zig");
 const patterns_outside = @import("patterns_outside.zig");
 
 const intro = @import("scenes/intro.zig");
+const menu = @import("scenes/menu.zig");
 /// Exported for calculations in JS (Width);
 export fn width() i32 {
     return renderer.WIDTH;
@@ -106,50 +107,6 @@ var player1 = Player{
     .color = colors.C64_BLUE,
 };
 
-const Cursor2DRaw = struct {
-    movedLast: u32,
-    pos: Point,
-    color: u32,
-    h: u32 = 24,
-    w: u32 = 24,
-};
-
-var menuCursor = Cursor2DRaw{ .movedLast = 0, .pos = Point{ .x = 24, .y = 8 }, .color = colors.C64_BLUE, .w = 8 * 35, .h = 24 };
-
-const WAIT = 16;
-fn tick_menu() void {
-    const buttons_lo = input_data.buttons_lo;
-    const buttons_hi = input_data.buttons_hi;
-    const JUMP = 32;
-    const MENU_MIN = 8 + JUMP;
-    const MENU_MAX = 8 * 20;
-    menuCursor.movedLast += 1;
-
-    if ((buttons_lo & input.BTN_UP) != 0 and menuCursor.pos.y >= MENU_MIN and menuCursor.movedLast > WAIT) {
-        menuCursor.movedLast = 0;
-        menuCursor.pos.y -= JUMP;
-    }
-    if ((buttons_lo & input.BTN_DOWN) != 0 and menuCursor.pos.y <= MENU_MAX and menuCursor.movedLast > WAIT) {
-        menuCursor.movedLast = 0;
-        menuCursor.pos.y += JUMP;
-    }
-
-    if ((buttons_hi & input.BTN_START) != 0) {
-        if (menuCursor.pos.y == 8 + (JUMP * 0)) {
-            console_log(1);
-        } else if (menuCursor.pos.y == 8 + (JUMP * 1)) {
-            console_log(2);
-        } else if (menuCursor.pos.y == 8 + (JUMP * 2)) {
-            console_log(3);
-        } else if (menuCursor.pos.y == 8 + (JUMP * 3)) {
-            console_log(4);
-        } else if (menuCursor.pos.y == 8 + (JUMP * 4)) {
-            console_log(5);
-        } else if (menuCursor.pos.y == 8 + (JUMP * 5)) {
-            console_log(6);
-        }
-    }
-}
 
 fn tick_old() void {
     const buttons_lo = input_data.buttons_lo;
@@ -227,7 +184,7 @@ fn tick_old() void {
 export fn tick() void {
     switch (scene.scene) {
         .intro => intro.tick(input_data),
-        .menu => tick_menu(),
+        .menu => menu.tick(input_data),
         .new => tick_old(), // or whatever "game" render is
         .load => {}, // placeholder
         .exit => {}, // placeholder
@@ -279,42 +236,13 @@ fn render_old() void {
     font.drawString(16, 32, "a retro game written in zig/wasm", colors.C64_BLACK, colors.C64_CYAN);
 }
 
-fn render_menu() void {
-    const BG = colors.C64_DARK_GRAY;
-    renderer.fillRect(0, 0, renderer.WIDTH, renderer.HEIGHT, BG);
-    font.drawString(24, 8 * 1, "                                   ", BG, colors.C64_YELLOW);
-    font.drawString(24, 8 * 2, " continue                          ", BG, colors.C64_YELLOW);
-    font.drawString(24, 8 * 3, "                                   ", BG, colors.C64_YELLOW);
-
-    font.drawString(24, 8 * 5, "                                   ", BG, colors.C64_BLUE);
-    font.drawString(24, 8 * 6, " load                              ", BG, colors.C64_BLUE);
-    font.drawString(24, 8 * 7, "                                   ", BG, colors.C64_BLUE);
-
-    font.drawString(24, 8 * 9, "                                   ", BG, colors.C64_GREEN);
-    font.drawString(24, 8 * 10, " new                               ", BG, colors.C64_GREEN);
-    font.drawString(24, 8 * 11, "                                   ", BG, colors.C64_GREEN);
-
-    font.drawString(24, 8 * 13, "                                   ", BG, colors.C64_ORANGE);
-    font.drawString(24, 8 * 14, " credits                           ", BG, colors.C64_ORANGE);
-    font.drawString(24, 8 * 15, "                                   ", BG, colors.C64_ORANGE);
-
-    font.drawString(24, 8 * 17, "                                   ", BG, colors.C64_PURPLE);
-    font.drawString(24, 8 * 18, " options                           ", BG, colors.C64_PURPLE);
-    font.drawString(24, 8 * 19, "                                   ", BG, colors.C64_PURPLE);
-
-    font.drawString(24, 8 * 21, "                                   ", BG, colors.C64_RED);
-    font.drawString(24, 8 * 22, " exit                              ", BG, colors.C64_RED);
-    font.drawString(24, 8 * 23, "                                   ", BG, colors.C64_RED);
-
-    renderer.drawRectOutline(menuCursor.pos.x, menuCursor.pos.y, menuCursor.w, menuCursor.h, colors.C64_WHITE);
-}
 
 
 /// Renders the current frame
 export fn render() void {
     switch (scene.scene) {
         .intro => intro.render(),
-        .menu => render_menu(),
+        .menu => menu.render(),
         .new => render_old(), // or whatever "game" render is
         .load => {}, // placeholder
         .exit => {}, // placeholder
---
+++ src/scenes/menu.zig
@@ -0,0 +1,81 @@
+const input = @import("../input.zig");
+const renderer = @import("../render.zig");
+const scene = @import("../scene.zig");
+const colors = @import("../colors.zig");
+const font = @import("../font.zig");
+const Cursor2DRaw = struct {
+    movedLast: u32,
+    x: u32,
+    y: u32,
+    color: u32,
+    h: u32 = 24,
+    w: u32 = 24,
+};
+
+const WAIT = 16;
+
+var menuCursor = Cursor2DRaw{ .movedLast = 0, .x = 24, .y = 8,  .color = colors.C64_BLUE, .w = 8 * 35, .h = 24 };
+
+pub fn tick(input_data: input.Layout) void {
+    const buttons_lo = input_data.buttons_lo;
+    const buttons_hi = input_data.buttons_hi;
+    const JUMP = 32;
+    const MENU_MIN = 8 + JUMP;
+    const MENU_MAX = 8 * 20;
+    menuCursor.movedLast += 1;
+
+    if ((buttons_lo & input.BTN_UP) != 0 and menuCursor.y >= MENU_MIN and menuCursor.movedLast > WAIT) {
+        menuCursor.movedLast = 0;
+        menuCursor.y -= JUMP;
+    }
+    if ((buttons_lo & input.BTN_DOWN) != 0 and menuCursor.y <= MENU_MAX and menuCursor.movedLast > WAIT) {
+        menuCursor.movedLast = 0;
+        menuCursor.y += JUMP;
+    }
+
+    if ((buttons_hi & input.BTN_START) != 0) {
+        if (menuCursor.y == 8 + (JUMP * 0)) {
+            scene.scene = .intro;
+        } else if (menuCursor.y == 8 + (JUMP * 1)) {
+            scene.scene = .intro;
+        } else if (menuCursor.y == 8 + (JUMP * 2)) {
+            scene.scene = .intro;
+        } else if (menuCursor.y == 8 + (JUMP * 3)) {
+            scene.scene = .intro;
+        } else if (menuCursor.y == 8 + (JUMP * 4)) {
+            scene.scene = .intro;
+        } else if (menuCursor.y == 8 + (JUMP * 5)) {
+            scene.scene = .intro;
+        }
+    }
+}
+
+pub fn render() void {
+    const BG = colors.C64_DARK_GRAY;
+    renderer.fillRect(0, 0, renderer.WIDTH, renderer.HEIGHT, BG);
+    font.drawString(24, 8 * 1, "                                   ", BG, colors.C64_YELLOW);
+    font.drawString(24, 8 * 2, " continue                          ", BG, colors.C64_YELLOW);
+    font.drawString(24, 8 * 3, "                                   ", BG, colors.C64_YELLOW);
+
+    font.drawString(24, 8 * 5, "                                   ", BG, colors.C64_BLUE);
+    font.drawString(24, 8 * 6, " load                              ", BG, colors.C64_BLUE);
+    font.drawString(24, 8 * 7, "                                   ", BG, colors.C64_BLUE);
+
+    font.drawString(24, 8 * 9, "                                   ", BG, colors.C64_GREEN);
+    font.drawString(24, 8 * 10, " new                               ", BG, colors.C64_GREEN);
+    font.drawString(24, 8 * 11, "                                   ", BG, colors.C64_GREEN);
+
+    font.drawString(24, 8 * 13, "                                   ", BG, colors.C64_ORANGE);
+    font.drawString(24, 8 * 14, " credits                           ", BG, colors.C64_ORANGE);
+    font.drawString(24, 8 * 15, "                                   ", BG, colors.C64_ORANGE);
+
+    font.drawString(24, 8 * 17, "                                   ", BG, colors.C64_PURPLE);
+    font.drawString(24, 8 * 18, " options                           ", BG, colors.C64_PURPLE);
+    font.drawString(24, 8 * 19, "                                   ", BG, colors.C64_PURPLE);
+
+    font.drawString(24, 8 * 21, "                                   ", BG, colors.C64_RED);
+    font.drawString(24, 8 * 22, " exit                              ", BG, colors.C64_RED);
+    font.drawString(24, 8 * 23, "                                   ", BG, colors.C64_RED);
+
+    renderer.drawRectOutline(menuCursor.x, menuCursor.y, menuCursor.w, menuCursor.h, colors.C64_WHITE);
+}
