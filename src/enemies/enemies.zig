91c62df9dc07a86368e900b8404160cb2796e1fe
Ulf Dellbrügge <xintamosaik@gmail.com>
2026-05-02 13:08:37 +0200

    add color to enemies

--- src/enemies/enemies.zig
+++ src/enemies/enemies.zig
@@ -1,24 +1,28 @@
 const weapons = @import("../weapons/weapons.zig");
 const pattern = @import("../patterns/patterns.zig");
 const pattern_enemies = @import("../patterns/enemy.zig");
+const colors = @import("../colors.zig");
 pub const Enemies = enum (u8) {
     wolf,
     goblin,
 };
 
 pub const Enemy = struct {
+    color: u32,
     kind: Enemies,
     weapon: weapons.Weapon,
     pattern: pattern.Pattern,
 };
 
 pub const wolf = Enemy{
+    .color = colors.C64_DARK_GRAY,
     .kind = .wolf,
     .weapon = weapons.fangs,
     .pattern = pattern_enemies.WOLF,
 };
 
 pub const goblin = Enemy{
+    .color = colors.C64_GREEN,
     .kind = .goblin,
     .weapon = weapons.club,
     .pattern = pattern_enemies.GOBLIN
